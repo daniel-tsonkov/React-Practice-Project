65bffb1a145ab2b2d47c84465e14c6185304ed18
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-03 19:01:44 +0200

    up

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,24 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 require('dotenv').config();
+
+//here we define evrithing needen for user
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {},
+    role: {},
+    firstname: {},
+    lastname: {},
+    age: {},
+    date: {},
+    verified: {}
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = { User };
