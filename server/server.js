6a03662497bb14b33cfb3307d8f6d1ca573eeb99
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-03 10:09:04 +0200

    03032025

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const bodyParser = require('body-parser'); //using as middleware
+
+const mongoUri = `mongodb+srv://${process.env.DB_HOST}`;
 
 //PARSING
 app.use(bodyParser.json());
