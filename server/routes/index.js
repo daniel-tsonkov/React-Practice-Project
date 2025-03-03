a81f70cefd8619222c3a4d33e5233037e7211a3b
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-03 20:44:32 +0200

    up

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 //routes
-const articlesRoute = require('./article.route');
+const articlesRoute = require('./articles.route');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 
