6c4d84af70fcc2c7757afccf4498f00da757c5ec
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-05 21:46:08 +0200

    up

---
+++ server/middleware/auth.js
@@ -0,0 +1,8 @@
+const passport = require('passport');
+const { ApiError } = require('./apiError');
+const httpStatus = require('http-status');
+
+
+const auth = () => async (req, res, next) => {
+
+}
