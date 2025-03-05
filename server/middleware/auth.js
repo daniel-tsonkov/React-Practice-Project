3d976051f32cc7443c9dab29c7d305d0ac401b21
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-05 21:57:59 +0200

    up

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,8 +1,16 @@
 const passport = require('passport');
 const { ApiError } = require('./apiError');
 const httpStatus = require('http-status');
+const { verify } = require('jsonwebtoken');
 
 
 const auth = () => async (req, res, next) => {
+    return new Promise((resolve, reject) => {
 
-}
+    })
+        .then(() => next())
+        .catch((err) => next(err))
+    passport.authenticate('jwt', { session: false }, verify(req, res))(req, res, next)
+};
+
+module.exports = auth;
