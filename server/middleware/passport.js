8ba554704388366abdf7479097f7d0f505f2cd05
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-05 10:03:57 +0200

    up

---
+++ server/middleware/passport.js
@@ -0,0 +1,20 @@
+//use this code becose is more new
+const { User } = require('../models/user');
+require('dotenv').config();
+
+const { Strategy: JwtStrategy, ExtractJwt, Strategy } = require('passport-jwt');
+
+const jwtOptions = {
+    secretOrKey: process.env.DB_SECRET,
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+}
+
+const jwtVerify = async () => {
+
+}
+
+const jwtStrategy = new Strategy(jwtOptions, jwtVerify);
+
+module.exports = {
+    jwtStrategy
+}
