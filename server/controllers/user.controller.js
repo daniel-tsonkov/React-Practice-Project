c58b706db65c20c65890185b0323a7c870346ecb
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-06 12:11:27 +0200

    up

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,11 +1,20 @@
 const httpStatus = require('http-status');
 const { ApiError } = require('../middleware/apiError');
 
+const { userService } = require('../services');
 
 const userController = {
     async profile(req, res, next) {
         try {
-            const user = await userService.findUserById();
+            const user = await userService.findUserById(req.user._id);
+
+            if (!user) {
+                throw new ApiError(httpStatus.NOT_FOUND, 'User not found!!!')
+            }
+
+            // res.json(user._doc);
+            // res.json(user);
+            res.json(res.locals.permission.filter(user._doc));
         } catch (err) {
             next(err);
         }
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -10,5 +10,6 @@ const findUserById = async (_id) => {
 }
 
 module.exports = {
-    findUserByEmail
+    findUserByEmail,
+    findUserById
 }
