1b312be6eb76423557d8802e87748fe8e6b8131c
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-07 09:05:35 +0200

    up

--- server/services/email.service.js
+++ server/services/email.service.js
@@ -2,3 +2,19 @@ const nodemailer = require('nodemailer');
 const Mailgen = require('mailgen');
 require('dotenv').config();
 
+let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    secure: true,
+    auth: {
+        user: process.env.EMAIL
+        //pass: ???????
+    }
+})
+
+const registerEmail = async () => {
+
+}
+
+module.exports = {
+    registerEmail
+}
