71aea9fc777626513b8cbf69581fa8e0826069b4
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-09 19:14:57 +0200

    up

--- server/services/articles.service.js
+++ server/services/articles.service.js
@@ -1,9 +1,15 @@
 const httpStatus = require('http-status');
-const { Category } = require('../models/category')
+const { Category } = require('../models/category');
+const { ApiError } = require('../middleware/apiError');
+
 
 const addCategory = async (body) => {
     try {
-
+        //validation
+        const category = new Category({
+            ...body
+        });
+        await category.save();
     } catch (err) {
         throw err;
     }
