d6f06cd670490d4d9749016dfe8101f090ac9234
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-10 17:00:51 +0200

    up

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -2,9 +2,29 @@ const { check, validationResult } = require('express-validator');
 const httpStatus = require('http-status');
 
 const addArticleValidator = [
-    check(''),
+    check('title')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('You need to add a title')
+        .bail()
+        .isLength({ min: 3 })
+        .withMessage('Minimum of 3 required'),
+    check('director')
+        .trim()
+        .not()
+        .isEmpty()
+        .withMessage('You need to add a director')
+        .bail()
+        .not()
+        .isBoolean()
+        .withMessage('You cannot add a boolean here')
+        .bail()
+        .isLength({ min: 3, max: 100 })
+        .withMessage('Minimum of 3 required')
+        .bail(),
     (req, res, next) => {
-
+        const errors = validationResult(req);
         next();
     }
 ]
