92498bd17f7bfc668fc0b0318a5fddaca08bf2ee
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-10 17:07:30 +0200

    up

--- server/middleware/validation.js
+++ server/middleware/validation.js
@@ -25,9 +25,14 @@ const addArticleValidator = [
         .bail(),
     (req, res, next) => {
         const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(httpStatus.BAD_REQUEST).json({
+                errors: errors.array()
+            });
+        };
         next();
     }
-]
+];
 
 module.exports = {
     addArticleValidator
