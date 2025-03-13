096cdc67ad1f4450901dc94319395d50786baee2
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-13 11:01:48 +0200

    up

--- frontend/src/components/navigation/sideNavigation.jsx
+++ frontend/src/components/navigation/sideNavigation.jsx
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+import DehazeIcon from '@mui/icons-material/Dehaze';
+import MailIcon from '@mui/icons-material/Mail';
+import HomeIcon from '@mui/icons-material/Home';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
 const SideDrawer = () => {
-  return <></>;
+  const [state, setState] = useState(false); //false sidebar start close
+
+  return (
+    <>
+      <DehazeIcon className="drawer_btn" onClick={() => setState(true)} />
+
+      <Drawer
+        anchor={'right'}
+        open={state}
+        onClose={() => setState(false)}
+      ></Drawer>
+    </>
+  );
 };
 
 export default SideDrawer;
