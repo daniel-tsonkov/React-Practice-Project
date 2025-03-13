0afbf89ab5a0726d020e20304572c5e4f1da02ac
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-13 10:12:27 +0200

    up

--- frontend/src/components/navigation/header.jsx
+++ frontend/src/components/navigation/header.jsx
@@ -1,5 +1,20 @@
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import SideDrawer from './sideNavigation';
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <>
+      <nav className="navbar fixed-top">
+        <Link
+          to="/"
+          className="navbar-brand d-flex align-items-center fredoka_ff"
+        >
+          Flickbase
+        </Link>
+        <SideDrawer />
+      </nav>
+    </>
+  );
 };
 
 export default Header;
