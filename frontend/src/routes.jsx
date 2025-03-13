0c37ed6953dedc93bcc1dcf45f2fce6947e090a5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-13 09:59:20 +0200

    up

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,5 +1,16 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import Home from './components/home';
+import Header from './components/navigation/header';
+
 const Router = () => {
-  return <>app</>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
