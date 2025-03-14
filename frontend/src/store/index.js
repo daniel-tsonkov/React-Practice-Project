bb18f601e7a864874cdd37a06cb698e72629af76
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-14 20:47:02 +0200

    14032025

---
+++ frontend/src/store/index.js
@@ -0,0 +1,6 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import UsersReducer from './reducers/users';
+import SiteReducer from './reducers/site';
+import NotificationsReducer from './reducers/notifications';
+import ArticlesReducer from './reducers/articles';
