ba1b3e5cbfe19bff890b6e0cf89f9cfcee5fd7df
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-15 19:29:07 +0200

    up

--- frontend/src/components/auth/index.jsx
+++ frontend/src/components/auth/index.jsx
@@ -2,13 +2,29 @@ import { useFormik } from 'formik';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Auth = () => {
   const [register, setRegister] = useState(false);
   let navigate = useNavigate();
   //redux logic
   const user = useSelector((state) => state.users);
+  const notifications = useSelector((state) => state.notifications);
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    initialValues: {
+      email: 'user5@asd.asd',
+      password: 'aassdd',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Enter email').email('Enter valid email'),
+      password: Yup.string().required('Enter password'),
+    }),
+    onSubmit: (values) => {
+      handleSubmit(values);
+    },
+  });
 
   return <>Auth!!!</>;
 };
