724e17fddd858659011da9d01bcc47c3b58398a5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-15 18:34:57 +0200

    up

--- frontend/src/components/auth/index.jsx
+++ frontend/src/components/auth/index.jsx
@@ -1,4 +1,15 @@
+import { useFormik } from 'formik';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
+import { useSelector } from 'react-redux';
+
 const Auth = () => {
+  const [register, setRegister] = useState(false);
+  let navigate = useNavigate();
+  //redux logic
+  const user = useSelector((state) => state.users);
+
   return <>Auth!!!</>;
 };
 
