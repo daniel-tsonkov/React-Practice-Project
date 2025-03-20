c31068193b56a446de32222f58b4dec1421507f1
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-20 19:23:57 +0200

    up

---
+++ frontend/src/components/dashboard/articles/index.jsx
@@ -0,0 +1,10 @@
+import { AdminTitle } from '../../../utils/tools';
+
+const AdminArticles = () => {
+  return (
+    <>
+      <AdminTitle title="Articles" />
+      Admin articles index
+    </>
+  );
+};
