451d1dbbeaafc5a9ed0c4a958f441641236e07f6
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-21 11:20:23 +0200

    up

--- frontend/src/components/dashboard/articles/edit_add/add.jsx
+++ frontend/src/components/dashboard/articles/edit_add/add.jsx
@@ -1,4 +1,5 @@
 import { useFormik, FieldArray, FormikProvider } from 'formik';
+import { formValues } from './validationSchema';
 
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
@@ -8,6 +9,15 @@ const AddArticle = () => {
   const articles = useSelector((state) => state.articles);
   const dispatch = useDispatch();
   let navigate = useNavigate();
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: formValues,
+    validationSchema: '',
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
 
   return (
     <>
---
+++ frontend/src/components/dashboard/articles/edit_add/validationSchema.jsx
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+export const formValues = {
+  title: '',
+  content: '',
+  excerpt: '',
+  score: '',
+  director: '',
+  actors: [],
+  status: 'draft',
+  category: '',
+};
