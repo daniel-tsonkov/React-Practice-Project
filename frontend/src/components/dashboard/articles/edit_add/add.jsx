0616ffd86ecc17d4815c929400ca6b3998ac2ce1
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-21 15:13:17 +0200

    up

--- frontend/src/components/dashboard/articles/edit_add/add.jsx
+++ frontend/src/components/dashboard/articles/edit_add/add.jsx
@@ -5,6 +5,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { AdminTitle } from '../../../../utils/tools';
 
+//MUI
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import Chip from '@mui/material/Chip';
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import FormHelperText from '@mui/material/FormHelperText';
+import InputLabel from '@mui/material/InputLabel';
+import AddIcon from '@mui/icons-material/Add';
+
 const AddArticle = () => {
   const articles = useSelector((state) => state.articles);
   const dispatch = useDispatch();
@@ -22,7 +37,9 @@ const AddArticle = () => {
   return (
     <>
       <AdminTitle title="Add articles" />
-      Add article
+      <form className="mt-3 article_form" onSubmit={formik.handleSubmit}>
+        <div className="form-group"></div>
+      </form>
     </>
   );
 };
