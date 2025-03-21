508ffd156d4585d080d20251b8d6caad25c89e7f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-21 08:48:06 +0200

    21032025

---
+++ frontend/src/components/dashboard/articles/edit_add/add.jsx
@@ -0,0 +1,12 @@
+import { AdminTitle } from '../../../utils/tools';
+
+const AddArticle = () => {
+  return (
+    <>
+      <AdminTitle title="Add articles" />
+      Add articles index
+    </>
+  );
+};
+
+export default AddArticle;
