3da06095e5b0ab5c34f36c72294dbe36f831d623
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-21 10:55:07 +0200

    up

--- frontend/src/components/dashboard/articles/edit_add/add.jsx
+++ frontend/src/components/dashboard/articles/edit_add/add.jsx
@@ -1,6 +1,14 @@
+import { useFormik, FieldArray, FormikProvider } from 'formik';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { AdminTitle } from '../../../../utils/tools';
 
 const AddArticle = () => {
+  const articles = useSelector((state) => state.articles);
+  const dispatch = useDispatch();
+  let navigate = useNavigate();
+
   return (
     <>
       <AdminTitle title="Add articles" />
