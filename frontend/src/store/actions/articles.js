8c49c4bd7b21ed0fc17057ad27103c463937f73a
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-23 19:23:10 +0200

    up

---
+++ frontend/src/store/actions/articles.js
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { errorGlobal, successGlobal } from '../reducers/notifications';
+import { getAuthHeader } from '../../utils/tools'
+
+import axios from 'axios';
+
+
+
+export const getCategories = createAsyncThunk(
+    'articles/getCategories',
+    async (object, { dispatch }) => {
+        try {
+            const request = await axios.get(`/api/articles/categories`);
+        } catch (err) {
+            dispatch(errorGlobal(err.response.data.message));
+            throw err;
+        }
+    }
+);
