82e5cb3cd035298cd813fadc088b4d7c2c1e8e2f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-25 21:20:08 +0200

    up

--- frontend/src/components/dashboard/articles/index.jsx
+++ frontend/src/components/dashboard/articles/index.jsx
@@ -1,14 +1,15 @@
-import { AdminTitle } from '../../../utils/tools'
-
+import { useSelector, useDispatch } from 'react-redux';
+import { AdminTitle } from '../../../utils/tools';
 
 const AdminArticles = () => {
+  const articles = useSelector();
 
-    return(
-        <>
-            <AdminTitle title="Articles"/>
-            admin articles index
-        </>
-    )
-}
+  return (
+    <>
+      <AdminTitle title="Articles" />
+      admin articles index
+    </>
+  );
+};
 
 export default AdminArticles;
