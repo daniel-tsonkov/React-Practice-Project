90b31e950edfbe2489f0e1fb6867d68988f6230c
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-03-26 08:44:20 +0200

    26032025

---
+++ frontend/src/components/dashboard/articles/paginate.jsx
@@ -0,0 +1,8 @@
+import { Table, Pagination } from 'react-bootstrap';
+import { Loader } from '../../../utils/tools';
+
+const PaginateComponent = ({}) => {
+  return <>Paginate</>;
+};
+
+export default PaginateComponent;
