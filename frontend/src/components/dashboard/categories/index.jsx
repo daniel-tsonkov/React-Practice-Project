74639bed5e563ec3ae27ffffa4b6df47982433a6
Daniel <daniel_tzonkov@abv.bg>
2025-04-01 22:17:41 +0300

    up

--- frontend/src/components/dashboard/categories/index.jsx
+++ frontend/src/components/dashboard/categories/index.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { AdminTitle } from "../../../utils/tools";
 import { Table, Row, Col } from "react-bootstrap";
 
+import { getCategories } from '../../../store/actions/articles';
+
 const AdminCategories = () => {
-    return (<>
+    const articles = useSelector(state => state.articles);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch])
+
+    return (
+    <>
    <AdminTitle title="Categories" />
    <Row>
     <Col>
@@ -12,7 +24,8 @@ const AdminCategories = () => {
         input
     </Col>
    </Row>
-    </>)
+    </>
+    )
 }
 
 export default AdminCategories;
