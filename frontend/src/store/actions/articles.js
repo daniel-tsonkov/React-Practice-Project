07e2fc3d5c36438cd551e94abe5c7e8e61d4a13b
Daniel <daniel_tzonkov@abv.bg>
2025-04-03 20:28:37 +0300

    up

--- frontend/src/store/actions/articles.js
+++ frontend/src/store/actions/articles.js
@@ -1,124 +1,159 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { errorGlobal, successGlobal } from '../reducers/notifications';
-import { getAuthHeader } from '../../utils/tools'
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { errorGlobal, successGlobal } from "../reducers/notifications";
+import { getAuthHeader } from "../../utils/tools";
 
-import axios from 'axios';
+import axios from "axios";
 
 export const addArticle = createAsyncThunk(
-    'articles/addArticle',
-    async (article, { dispatch }) => {
-        try {
-            const request = await axios.post(`/api/articles`, article, getAuthHeader());
-            dispatch(successGlobal('Post created!!!'))
-            return request.data;
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+  "articles/addArticle",
+  async (article, { dispatch }) => {
+    try {
+      const request = await axios.post(
+        `/api/articles`,
+        article,
+        getAuthHeader()
+      );
+      dispatch(successGlobal("Post created!!!"));
+      return request.data;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
-
+  }
+);
 
 export const updateArticle = createAsyncThunk(
-    'articles/updateArticle',
-    async ({ values, articleId }, { dispatch }) => {
-        try {
-            await axios.patch(`/api/articles/article/${articleId}`, values, getAuthHeader())
-            dispatch(successGlobal('Article updated !!'))
-            return true
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+  "articles/updateArticle",
+  async ({ values, articleId }, { dispatch }) => {
+    try {
+      await axios.patch(
+        `/api/articles/article/${articleId}`,
+        values,
+        getAuthHeader()
+      );
+      dispatch(successGlobal("Article updated !!"));
+      return true;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
-
+  }
+);
 
 export const getAdminArticle = createAsyncThunk(
-    'articles/getAdminArticle',
-    async (_id, { dispatch }) => {
-        try {
-            const request = await axios.get(`/api/articles/article/${_id}`, getAuthHeader());
-            return request.data;
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+  "articles/getAdminArticle",
+  async (_id, { dispatch }) => {
+    try {
+      const request = await axios.get(
+        `/api/articles/article/${_id}`,
+        getAuthHeader()
+      );
+      return request.data;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
-
+  }
+);
 
 export const getPaginateArticles = createAsyncThunk(
-    'articles/getPaginateArticles',
-    async ({ page = 1, limit = 4, keywords = '' }, { dispatch }) => {
-        try {
-            const request = await axios.post(`/api/articles/admin/paginate`, {
-                page,
-                limit,
-                keywords
-            }, getAuthHeader());
-            return request.data;
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+  "articles/getPaginateArticles",
+  async ({ page = 1, limit = 4, keywords = "" }, { dispatch }) => {
+    try {
+      const request = await axios.post(
+        `/api/articles/admin/paginate`,
+        {
+          page,
+          limit,
+          keywords,
+        },
+        getAuthHeader()
+      );
+      return request.data;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
+  }
+);
 
 export const changeStatusArticle = createAsyncThunk(
-    'articles/changeStatusArticle',
-    async ({ newStatus, _id }, { dispatch, getState }) => {
-        try {
-            const request = await axios.patch(`/api/articles/article/${_id}`, {
-                status: newStatus
-            }, getAuthHeader());
+  "articles/changeStatusArticle",
+  async ({ newStatus, _id }, { dispatch, getState }) => {
+    try {
+      const request = await axios.patch(
+        `/api/articles/article/${_id}`,
+        {
+          status: newStatus,
+        },
+        getAuthHeader()
+      );
 
-            let article = request.data;
-            /// previous state
-            let state = getState().articles.adminArticles.docs;
-            /// find the position
-            let position = state.findIndex(article => article._id === _id);
-            /// We CANNOT mutate 'let state'.
-            const newState = [...state];
-            newState[position] = article;
+      let article = request.data;
+      /// previous state
+      let state = getState().articles.adminArticles.docs;
+      /// find the position
+      let position = state.findIndex((article) => article._id === _id);
+      /// We CANNOT mutate 'let state'.
+      const newState = [...state];
+      newState[position] = article;
 
-            dispatch(successGlobal('Status changed !!'));
-            return newState;
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+      dispatch(successGlobal("Status changed !!"));
+      return newState;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
-
+  }
+);
 
 export const removeArticle = createAsyncThunk(
-    'articles/removeArticle',
-    async (_id, { dispatch, getState }) => {
-        try {
-            await axios.delete(`/api/articles/article/${_id}`, getAuthHeader());
-            dispatch(successGlobal('Article Removed'));
+  "articles/removeArticle",
+  async (_id, { dispatch, getState }) => {
+    try {
+      await axios.delete(`/api/articles/article/${_id}`, getAuthHeader());
+      dispatch(successGlobal("Article Removed"));
 
-            let page = getState().articles.adminArticles.page;
-            dispatch(getPaginateArticles({ page }));
-            return true;
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+      let page = getState().articles.adminArticles.page;
+      dispatch(getPaginateArticles({ page }));
+      return true;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
-
+  }
+);
 
 export const getCategories = createAsyncThunk(
-    'articles/getCategories',
-    async (obj, { dispatch }) => {
-        try {
-            const request = await axios.get(`/api/articles/categories`, getAuthHeader());
-            return request.data;
-        } catch (error) {
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+  "articles/getCategories",
+  async (obj, { dispatch }) => {
+    try {
+      const request = await axios.get(
+        `/api/articles/categories`,
+        getAuthHeader()
+      );
+      return request.data;
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
+  }
+);
+
+export const addCategory = createAsyncThunk(
+  "articles/addCategory",
+  async (data, { dispatch, getState }) => {
+    try {
+      const category = await axios.post(
+        "api/articles/categories",
+        data,
+        getAuthHeader()
+      );
+
+      const state = getState()
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
+    }
+  }
+);
