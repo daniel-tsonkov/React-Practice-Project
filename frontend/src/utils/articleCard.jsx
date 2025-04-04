91802ad6d7d21a0ac069cf1a5bca5c7569ecb2c8
Daniel <daniel_tzonkov@abv.bg>
2025-04-04 10:54:24 +0300

    up

--- frontend/src/utils/articleCard.jsx
+++ frontend/src/utils/articleCard.jsx
@@ -18,6 +18,22 @@ const ArticleCard = ({ article }) => {
         image={`https://picsum.photos/200?${article._id}`}
         title="some title"
       />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {article.title}
+        </Typography>
+        <Typography variant="body2" component="p">
+          {article.excerpt}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton>
+          <FavoriteIcon />
+        </IconButton>
+        <Button size="small" color="primary" component={RouterLink} to={`/articles/article/${article._id}`}>
+          View article
+        </Button>
+      </CardActions>
     </Card>
   );
 };
