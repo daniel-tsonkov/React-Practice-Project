e2c2bf0feb005c6517e400fd6822ba3ca61e971e
Daniel <daniel_tzonkov@abv.bg>
2025-04-06 00:09:21 +0300

    06042025

--- frontend/src/components/articles/article.jsx
+++ frontend/src/components/articles/article.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getArticle } from "../../store/actions/articles";
+import { Loader } from "../../utils/tools";
 
 const Article = () => {
   const articles = useSelector((state) => state.articles);
@@ -12,7 +13,28 @@ const Article = () => {
     dispatch(getArticle(id));
   }, id);
 
-  return <></>;
+  return (
+    <>
+      {articles && articles.current ? (
+        <div className="article_container">
+          <div
+            style={{
+              background: `url(https://picsum.photos/1920/1080)`,
+            }}
+            className="image"
+          ></div>
+          <h1>{articles.current.title}</h1>
+          <div className="mt-3 content">
+            <div
+              dangerouslySetInnerHTML={{ __html: articles.current.content }}
+            ></div>
+          </div>
+        </div>
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
 };
 
 export default Article;
