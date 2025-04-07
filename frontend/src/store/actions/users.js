5dc2fe44220dd60235c98669de15bc1a6f9064b5
Daniel <daniel_tzonkov@abv.bg>
2025-04-07 09:45:09 +0300

    up

--- frontend/src/store/actions/users.js
+++ frontend/src/store/actions/users.js
@@ -1,76 +1,73 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { errorGlobal, successGlobal } from '../reducers/notifications';
-import { getAuthHeader, removeTokenCookie } from '../../utils/tools'
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { errorGlobal, successGlobal } from "../reducers/notifications";
+import { getAuthHeader, removeTokenCookie } from "../../utils/tools";
 
-import axios from 'axios';
+import axios from "axios";
+import { setVerify } from "../reducers/users";
 
 export const registerUser = createAsyncThunk(
-    'users/registerUser',
-    async({email,password},{dispatch})=>{
-        try{
-            const request = await axios.post(`/api/auth/register`,{
-                email:email,
-                password:password
-            });
+  "users/registerUser",
+  async ({ email, password }, { dispatch }) => {
+    try {
+      const request = await axios.post(`/api/auth/register`, {
+        email: email,
+        password: password,
+      });
 
-            dispatch(successGlobal('Welcome !!! Check your emails to validate '))
-            return {data:request.data.user,auth:true}
-        }catch(error){
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+      dispatch(successGlobal("Welcome !!! Check your emails to validate "));
+      return { data: request.data.user, auth: true };
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
-
-
+  }
+);
 
 export const signInUser = createAsyncThunk(
-    'users/signInUser',
-    async({email,password},{dispatch})=>{
-        try{
-            const request = await axios.post(`/api/auth/signin`,{
-                email:email,
-                password:password
-            })
+  "users/signInUser",
+  async ({ email, password }, { dispatch }) => {
+    try {
+      const request = await axios.post(`/api/auth/signin`, {
+        email: email,
+        password: password,
+      });
 
-            /// show a message
-            dispatch(successGlobal('Welcome !'))
-            return {data:request.data.user,auth:true}
-        }catch(error){
-            dispatch(errorGlobal(error.response.data.message))
-            throw error;
-        }
+      /// show a message
+      dispatch(successGlobal("Welcome !"));
+      return { data: request.data.user, auth: true };
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
+  }
+);
 
+export const isAuth = createAsyncThunk("users/isAuth", async () => {
+  try {
+    const request = await axios.get("/api/auth/isauth", getAuthHeader());
+    return { data: request.data, auth: true };
+  } catch (error) {
+    return { data: {}, auth: false };
+  }
+});
 
-export const isAuth = createAsyncThunk(
-    'users/isAuth',
-    async()=>{
-        try{
-            const request = await axios.get('/api/auth/isauth',getAuthHeader())
-            return { data: request.data, auth: true}
-        }catch(error){
-            return { data: {}, auth: false}
-        }
-    }
-)
-
-
-export const signOut = createAsyncThunk(
-    'users/signOut',
-    async()=>{
-        removeTokenCookie();
-    }
-)
+export const signOut = createAsyncThunk("users/signOut", async () => {
+  removeTokenCookie();
+});
 
 export const accountVerify = createAsyncThunk(
-    'users/accountVerify',
-    async()=>{
-        try{
-            
-        }catch(error){
-            throw error;
-        }
+  "users/accountVerify",
+  async (token, { dispatch, getState }) => {
+    try {
+      const user = getState().users.auth;
+      await axios.get(`/api/users/verify?validation=${token}`);
+
+      if (user) {
+        dispatch(setVerify());
+      }
+    } catch (error) {
+      dispatch(errorGlobal(error.response.data.message));
+      throw error;
     }
-)
+  }
+);
--- frontend/src/store/reducers/users.js
+++ frontend/src/store/reducers/users.js
@@ -1,65 +1,73 @@
-import { createSlice } from '@reduxjs/toolkit';
-import {
-    registerUser,
-    signInUser,
-    isAuth,
-    signOut
-} from '../actions/users'
+import { createSlice } from "@reduxjs/toolkit";
+import { registerUser, signInUser, isAuth, signOut } from "../actions/users";
 
 let DEFAULT_USER_STATE = {
-    loading:false,
-    data:{
-        _id:null,
-        email:null,
-        firstname:null,
-        lastname:null,
-        age:null,
-        role:null,
-        verified:null
-    },
-    auth:null
-}
-
+  loading: false,
+  data: {
+    _id: null,
+    email: null,
+    firstname: null,
+    lastname: null,
+    age: null,
+    role: null,
+    verified: null,
+  },
+  auth: null,
+};
 
 export const usersSlice = createSlice({
-    name:'users',
-    initialState:DEFAULT_USER_STATE,
-    reducers:{
-
+  name: "users",
+  initialState: DEFAULT_USER_STATE,
+  reducers: {
+    setVerify: (state) => {
+      state.data.verified = true;
     },
-    extraReducers:(builder)=>{
-        builder
-        // REGISTER
-        .addCase(registerUser.pending,(state)=>{ state.loading = true })
-        .addCase(registerUser.fulfilled,(state,action)=>{
-            state.loading = false;
-            state.data = action.payload.data;
-            state.auth = action.payload.auth;
-        })
-        .addCase(registerUser.rejected,(state)=>{ state.loading = false })
-        //SIGN IN
-        .addCase(signInUser.pending,(state)=>{ state.loading = true })
-        .addCase(signInUser.fulfilled,(state,action)=>{
-            state.loading = false;
-            state.data = action.payload.data;
-            state.auth = action.payload.auth;
-        })
-        .addCase(signInUser.rejected,(state)=>{ state.loading = false })
-        /// IS AUTH
-        .addCase(isAuth.pending,(state)=>{ state.loading = true })
-        .addCase(isAuth.fulfilled,(state,action)=>{
-            state.loading = false;
-            state.data = {...state.data,...action.payload.data}
-            state.auth = action.payload.auth;
-        })
-        .addCase(isAuth.rejected,(state)=>{ state.loading = false })
-        // SIGN OUT
-        .addCase(signOut.fulfilled,(state)=>{
-            state.data = DEFAULT_USER_STATE.data;
-            state.auth = false;
-        })
-    }
+  },
+  extraReducers: (builder) => {
+    builder
+      // REGISTER
+      .addCase(registerUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload.data;
+        state.auth = action.payload.auth;
+      })
+      .addCase(registerUser.rejected, (state) => {
+        state.loading = false;
+      })
+      //SIGN IN
+      .addCase(signInUser.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(signInUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = action.payload.data;
+        state.auth = action.payload.auth;
+      })
+      .addCase(signInUser.rejected, (state) => {
+        state.loading = false;
+      })
+      /// IS AUTH
+      .addCase(isAuth.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(isAuth.fulfilled, (state, action) => {
+        state.loading = false;
+        state.data = { ...state.data, ...action.payload.data };
+        state.auth = action.payload.auth;
+      })
+      .addCase(isAuth.rejected, (state) => {
+        state.loading = false;
+      })
+      // SIGN OUT
+      .addCase(signOut.fulfilled, (state) => {
+        state.data = DEFAULT_USER_STATE.data;
+        state.auth = false;
+      });
+  },
 });
 
-/// action......
+export const { setVerify } = usersSlice.actions;
 export default usersSlice.reducer;
