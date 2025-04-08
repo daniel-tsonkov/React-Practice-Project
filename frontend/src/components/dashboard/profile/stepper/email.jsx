3112a087dd926bcbf42605a7965d26333f08cd3d
Daniel <daniel_tzonkov@abv.bg>
2025-04-08 20:37:12 +0300

    up

--- frontend/src/components/dashboard/profile/stepper/email.jsx
+++ frontend/src/components/dashboard/profile/stepper/email.jsx
@@ -1,4 +1,12 @@
-const EmailStepper = () => {
+import { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import { errorHelper, Loader } from "../../../../utils/tools";
+
+import { TextField, Button, Stepper, Step, StepLabel } from "@mui/material";
+
+const EmailStepper = ({ user, closeModal }) => {
   return <>stepper</>;
 };
 
