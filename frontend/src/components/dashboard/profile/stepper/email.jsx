c8d2dc27fba91f9f09053b3ff83bd9a6b3b61fc5
Daniel <daniel_tzonkov@abv.bg>
2025-04-08 20:43:40 +0300

    up

--- frontend/src/components/dashboard/profile/stepper/email.jsx
+++ frontend/src/components/dashboard/profile/stepper/email.jsx
@@ -7,6 +7,48 @@ import { errorHelper, Loader } from "../../../../utils/tools";
 import { TextField, Button, Stepper, Step, StepLabel } from "@mui/material";
 
 const EmailStepper = ({ user, closeModal }) => {
+  const [activeStep, setActiveStep] = useState(0);
+  const steps = ["Enter old email", "Enter new email", "Are you sure ?"];
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: { email: "", newemail: "" },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const nextBtn = () => {
+    <Button
+      className="mt-3"
+      variant="contained"
+      color="primary"
+      onClick={handleNext}
+    >
+      Next
+    </Button>;
+  };
+
+  const backBtn = () => {
+    <Button
+      className="mt-3"
+      variant="contained"
+      color="primary"
+      onClick={handleBack}
+    >
+      Back
+    </Button>;
+  };
+
   return <>stepper</>;
 };
 
